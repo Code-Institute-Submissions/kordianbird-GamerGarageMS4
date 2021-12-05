69737aa9a476c510058a8fc716e23e9bf49d1b08
kordian ptak <kordianbird@Gmail.com>
2021-12-05 15:44:21 +0000

    products can now be incremented, decremented in cart

--- cart/views.py
+++ cart/views.py
@@ -9,28 +9,48 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    """ View that adds item to cart """
+    """ Add a quantity of the specified product to the shopping cart """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if size:
+        if item_id in list(cart.keys()):
+            if size in cart[item_id]['items_by_size'].keys():
+                cart[item_id]['items_by_size'][size] += quantity
+            else:
+                cart[item_id]['items_by_size'][size] = quantity
+        else:
+            cart[item_id] = {'items_by_size': {size: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     return redirect(redirect_url)
 
 
 def remove_from_cart(request, item_id):
-    """ View that removes item from cart """
+    """Remove the item from the shopping cart"""
 
     try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
         cart = request.session.get('cart', {})
 
-        cart.pop(item_id)
+        if size:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
@@ -39,17 +59,29 @@ def remove_from_cart(request, item_id):
         return HttpResponse(status=500)
 
 
+
 def adjust_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
-
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[item_id] = quantity
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+        else:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
     else:
-        cart.pop(item_id)
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
