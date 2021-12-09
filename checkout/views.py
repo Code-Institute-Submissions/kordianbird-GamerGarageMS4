57f485d518d80796d08da770734fa11fe58a0de7
kordian ptak <kordianbird@Gmail.com>
2021-12-09 01:29:16 +0000

    added stripe

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K4bDAKvloDPj9mLnLK7RLpVRU2gccxuhyRcoV2tiSD6rTUrftNyCX8tn8zcNgZfxtzOp719nFToCrTrd8mZS6AY00Du2BU3AW',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
