de72832eb4084e8b3fb831a36af1ccd7c3c6dec4
kordian ptak <kordianbird@Gmail.com>
2021-12-15 22:34:00 +0000

    erased whitespace

--- gamergarage/settings.py
+++ gamergarage/settings.py
@@ -184,7 +184,7 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-    
+
     AWS_STORAGE_BUCKET_NAME = 'gamergarage-kordian'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
