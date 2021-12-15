36eb71d756c55b8a3239d8d7f14bccc9dbe308ec
kordian ptak <kordianbird@Gmail.com>
2021-12-15 20:45:34 +0000

    removed secret key

--- gamergarage/settings.py
+++ gamergarage/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-skw91iwq==_)z6_#@6pdz-=3nun-#^=p--ktd6-y)$3v=1l0c5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gamergarage-kordian.herokuapp.com', 'localhost']
 
