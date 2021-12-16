a7dfc2f61a1e13c8e5fbfa0b1bf4fd2660b2c91f
kordian ptak <kordianbird@Gmail.com>
2021-12-16 18:07:39 +0000

    manually collected static

--- gamergarage/settings.py
+++ gamergarage/settings.py
@@ -48,8 +48,6 @@ INSTALLED_APPS = [
     'cart',
     'checkout',
     'profiles',
-
-    # other
     'crispy_forms',
     'storages',
 ]
@@ -171,7 +169,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
